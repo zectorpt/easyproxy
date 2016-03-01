ecc575d2c7c398dc33acaebdc1d87a9a8237c2a7
Jose Almeida <zectorpt@JoseAlmeidaLAP.ocado.com>
2016-03-01 12:16:31 +0000

    Comments and Stuff

---
+++ server/menu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
+display_result() {
+  dialog --title "$1" \
+    --no-collapse \
+    --msgbox "$result" 0 0
+}
+while true; do
+  exec 3>&1
+  selection=$(dialog \
+    --backtitle "System Information" \
+    --title "Menu" \
+    --clear \
+    --cancel-label "Exit" \
+    --menu "Please select:" $HEIGHT $WIDTH 4 \
+    "1" "Xclock - Unix Clock" \
+    "2" "Display Disk Space" \
+    "3" "Display Home Space Utilization" \
+    2>&1 1>&3)
+  exit_status=$?
+  exec 3>&-
+  case $exit_status in
+    $DIALOG_CANCEL)
+      clear
+      echo "Program terminated."
+      exit
+      ;;
+    $DIALOG_ESC)
+      clear
+      echo "Program aborted." >&2
+      exit 1
+      ;;
+  esac
+  case $selection in
+    0 )
+      clear
+      echo "Program terminated."
+      ;;
+    1 )
+      echo "Best way to test X-Window"
+      result=$(xclock 2> /dev/null &)
+      sleep 3
+      ;;
+    2 )
+      echo "Opening Google Chrome... Is a little bit slow... Wait!"
+      result=$(google-chrome-stable 2> /dev/null &)
+      sleep 15
+      ;;
+    3 )
+      if [[ $(id -u) -eq 0 ]]; then
+        result=$(du -sh /home/* 2> /dev/null)
+        display_result "Home Space Utilization (All Users)"
+      else
+        result=$(du -sh $HOME 2> /dev/null)
+        display_result "Home Space Utilization ($USER)"
+      fi
+      ;;
+  esac
+done
